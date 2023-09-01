ba36a08461a50638e406b71614724223f32cabd6
YanSchw <102901459+YanSchw@users.noreply.github.com>
2023-09-01 15:44:27 +0200

    New Template Code and Content

--- Templates/EmptyProjectCPP/Code/Modules/MyModule/Source/MyGameInstance.h
+++ Templates/EmptyProjectCPP/Code/Modules/MyModule/Source/MyGameInstance.h
@@ -1,9 +1,11 @@
 #pragma once
+#include "CoreMinimal.h"
 #include "Suora/GameFramework/GameInstance.h"
+#include "MyGameInstance.generated.h"
 
 class MyGameInstance : public GameInstance
 {
     SUORA_CLASS(5438795711);
 public:
     
-}
+};
