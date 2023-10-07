bab00bd37281736a44b1ab15d9fecc94fdcfd724
YanSchw <102901459+YanSchw@users.noreply.github.com>
2023-10-07 13:05:31 +0200

    Removed ShaderLibrary

--- Code/Engine/Source/Suora/Renderer/Shader.cpp
+++ Code/Engine/Source/Suora/Renderer/Shader.cpp
@@ -89,41 +89,4 @@ namespace Suora
 		return shaderSources;
 	}
 
-	void ShaderLibrary::Add(const std::string& name, const Ref<Shader>& shader)
-	{
-		SUORA_ASSERT(!Exists(name), "Shader already exists!");
-		m_Shaders[name] = shader;
-	}
-
-	void ShaderLibrary::Add(const Ref<Shader>& shader)
-	{
-		auto& name = shader->GetName();
-		Add(name, shader);
-	}
-
-	Ref<Shader> ShaderLibrary::Load(const std::string& filepath)
-	{
-		auto shader = Shader::Create(filepath);
-		Add(shader);
-		return shader;
-	}
-
-	Ref<Shader> ShaderLibrary::Load(const std::string& name, const std::string& filepath)
-	{
-		auto shader = Shader::Create(filepath);
-		Add(name, shader);
-		return shader;
-	}
-
-	Ref<Shader> ShaderLibrary::Get(const std::string& name)
-	{
-		SUORA_ASSERT(Exists(name), "Shader not found!");
-		return m_Shaders[name];
-	}
-
-	bool ShaderLibrary::Exists(const std::string& name) const
-	{
-		return m_Shaders.find(name) != m_Shaders.end();
-	}
-
 }
--- Code/Engine/Source/Suora/Renderer/Shader.h
+++ Code/Engine/Source/Suora/Renderer/Shader.h
@@ -37,19 +37,4 @@ namespace Suora
 		static std::unordered_map<std::string, std::string> PreProcess(const std::string& source);
 	};
 
-	class ShaderLibrary
-	{
-	public:
-		void Add(const std::string& name, const Ref<Shader>& shader);
-		void Add(const Ref<Shader>& shader);
-		Ref<Shader> Load(const std::string& filepath);
-		Ref<Shader> Load(const std::string& name, const std::string& filepath);
-
-		Ref<Shader> Get(const std::string& name);
-
-		bool Exists(const std::string& name) const;
-	private:
-		std::unordered_map<std::string, Ref<Shader>> m_Shaders;
-	};
-
 }
