86a79e4604f19d0f6dff7e81a20c165926d80138
YanSchw <102901459+YanSchw@users.noreply.github.com>
2023-11-02 23:25:05 +0100

    Quick fix

--- Code/Runtime/Source/RuntimeApp.cpp
+++ Code/Runtime/Source/RuntimeApp.cpp
@@ -11,91 +11,95 @@
 
 extern void Modules_Init();
 
-class Runtime : public Suora::Application
+namespace Suora
 {
-public:
-	Suora::Ref<Framebuffer> m_Framebuffer;
-	Suora::Ref<Window> m_StandaloneWindow;
 
-	Runtime()
+	class Runtime : public Application
 	{
-		Modules_Init();
-		
-		m_StandaloneWindow = Suora::Ref<Window>(CreateAppWindow(WindowProps()));
+	public:
+		Ref<Framebuffer> m_Framebuffer;
+		Ref<Window> m_StandaloneWindow;
 
-		GraphicsContext* context = (GraphicsContext*)(m_StandaloneWindow->GetGraphicsContext());
-		context->MakeCurrent();
-
+		Runtime()
 		{
-			FramebufferSpecification spec;
-			spec.Width = 1920;
-			spec.Height = 1080;
-			spec.Attachments.Attachments.push_back(FramebufferTextureFormat::RGBA8);
-			spec.Attachments.Attachments.push_back(FramebufferTextureFormat::Depth);
-			m_Framebuffer = Framebuffer::Create(spec);
-		}
-		AssetManager::HotReload();
-		SuoraVerify(ProjectSettings::Get());
+			Modules_Init();
 
-		m_StandaloneWindow->SetTitle(ProjectSettings::GetProjectName());
+			m_StandaloneWindow = Ref<Window>(CreateAppWindow(WindowProps()));
 
-		Engine::Get()->CreateGameInstance();
-		if (Level* defaultLevel = ProjectSettings::Get()->m_DefaultLevel)
-		{
-			World* world = Engine::Get()->GetGameInstance()->LoadLevel(defaultLevel);
-			Engine::Get()->GetGameInstance()->SwitchToWorld(world);
-		}
-		else
-		{
-			SUORA_ERROR(LogCategory::Gameplay, "Could not load Default Level!");
-		}
-		m_StandaloneWindow->SetCursorLocked(true);
-		//m_StandaloneWindow->SetFullscreen(true);
-		m_StandaloneWindow->SetVSync(false);
-	}
+			GraphicsContext* context = (GraphicsContext*)(m_StandaloneWindow->GetGraphicsContext());
+			context->MakeCurrent();
 
-	~Runtime()
-	{
-	}
-
-	void Update(float deltaTime) override
-	{
-		m_StandaloneWindow->OnUpdate();
-
-		// Update the UI Viewport
-		UINode::s_UIViewportWidth = m_StandaloneWindow->GetWidth();
-		UINode::s_UIViewportHeight = m_StandaloneWindow->GetHeight();
-
-		// Rendering
-		SuoraAssert(Engine::Get());
-		if (GameInstance* game = Engine::Get()->GetGameInstance())
-		{
-			if (World* world = game->GetCurrentWorld())
 			{
-				if (CameraNode* camera = world->GetMainCamera())
-				{
-					camera->SetAspectRatio((float)m_StandaloneWindow->GetWidth() / (float)m_StandaloneWindow->GetHeight());
-					camera->SetPerspectiveFarClip(camera->GetPerspectiveFarClip());
+				FramebufferSpecification spec;
+				spec.Width = 1920;
+				spec.Height = 1080;
+				spec.Attachments.Attachments.push_back(FramebufferTextureFormat::RGBA8);
+				spec.Attachments.Attachments.push_back(FramebufferTextureFormat::Depth);
+				m_Framebuffer = Framebuffer::Create(spec);
+			}
+			AssetManager::HotReload();
+			SuoraVerify(ProjectSettings::Get());
 
-					RenderingParams RParams;
-					Engine::Get()->GetRenderPipeline()->Render(*m_Framebuffer.get(), *world, *camera, RParams);
+			m_StandaloneWindow->SetTitle(ProjectSettings::GetProjectName());
+
+			Engine::Get()->CreateGameInstance();
+			if (Level* defaultLevel = ProjectSettings::Get()->m_DefaultLevel)
+			{
+				World* world = Engine::Get()->GetGameInstance()->LoadLevel(defaultLevel);
+				Engine::Get()->GetGameInstance()->SwitchToWorld(world);
+			}
+			else
+			{
+				SUORA_ERROR(LogCategory::Gameplay, "Could not load Default Level!");
+			}
+			m_StandaloneWindow->SetCursorLocked(true);
+			//m_StandaloneWindow->SetFullscreen(true);
+			m_StandaloneWindow->SetVSync(false);
+		}
+
+		~Runtime()
+		{
+		}
+
+		void Update(float deltaTime) override
+		{
+			m_StandaloneWindow->OnUpdate();
+
+			// Update the UI Viewport
+			UINode::s_UIViewportWidth = m_StandaloneWindow->GetWidth();
+			UINode::s_UIViewportHeight = m_StandaloneWindow->GetHeight();
+
+			// Rendering
+			SuoraAssert(Engine::Get());
+			if (GameInstance* game = Engine::Get()->GetGameInstance())
+			{
+				if (World* world = game->GetCurrentWorld())
+				{
+					if (CameraNode* camera = world->GetMainCamera())
+					{
+						camera->SetAspectRatio((float)m_StandaloneWindow->GetWidth() / (float)m_StandaloneWindow->GetHeight());
+						camera->SetPerspectiveFarClip(camera->GetPerspectiveFarClip());
+
+						RenderingParams RParams;
+						Engine::Get()->GetRenderPipeline()->Render(*m_Framebuffer.get(), *world, *camera, RParams);
+					}
 				}
 			}
+			m_Framebuffer->Bind();
+			m_Framebuffer->Unbind();
+
+			RenderCommand::SetViewport(0, 0, m_StandaloneWindow->GetWidth(), m_StandaloneWindow->GetHeight());
+			m_Framebuffer->DrawToScreen(0, 0, m_StandaloneWindow->GetWidth(), m_StandaloneWindow->GetHeight());
+
+			if (NativeInput::GetKey(Key::RightAlt) && NativeInput::GetKeyDown(Key::Enter))
+			{
+				m_StandaloneWindow->SetFullscreen(!m_StandaloneWindow->IsFullscreen());
+				m_StandaloneWindow->SetVSync(true);
+			}
 		}
-		m_Framebuffer->Bind();
-		m_Framebuffer->Unbind();
 
-		RenderCommand::SetViewport(0, 0, m_StandaloneWindow->GetWidth(), m_StandaloneWindow->GetHeight());
-		m_Framebuffer->DrawToScreen(0, 0, m_StandaloneWindow->GetWidth(), m_StandaloneWindow->GetHeight());
-
-		if (NativeInput::GetKey(Key::RightAlt) && NativeInput::GetKeyDown(Key::Enter))
-		{
-			m_StandaloneWindow->SetFullscreen(!m_StandaloneWindow->IsFullscreen());
-			m_StandaloneWindow->SetVSync(true);
-		}
-	}
-
-};
+	};
+}
 
 Suora::Application* Suora::CreateApplication()
 {
