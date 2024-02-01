6b8057aa93db209b6937b8712a0db10e60aa16d0
YanSchw <schoessow.yannick@gmail.com>
2024-02-02 00:39:54 +0100

    Added GenerateVisualStudioSolution() for C#

--- Code/Modules/CSharpScripting/Source/CSharpProjectGenerator.cpp
+++ Code/Modules/CSharpScripting/Source/CSharpProjectGenerator.cpp
@@ -2,14 +2,15 @@
 #include "CoreMinimal.h"
 #include "Suora/Platform/Platform.h"
 #include "Suora/Assets/AssetManager.h"
-
+#include "Suora/Assets/SuoraProject.h"
+#include "Suora/Common/Random.h"
 
 namespace Suora
 {
 
 	static void GenerateSingleCSProj(const std::filesystem::path& filePath)
 	{
-        std::string compilerSources = "";
+        String compilerSources = "";
         for (auto It : std::filesystem::recursive_directory_iterator(filePath))
         {
             if (It.is_directory())
@@ -21,7 +22,7 @@ namespace Suora
             }
         }
 
-		std::string str = "\
+		String str = "\
 <Project Sdk=\"Microsoft.NET.Sdk\">\n\
   <PropertyGroup>\n\
     <OutputType>Library</OutputType>\n\
@@ -133,6 +134,114 @@ namespace Suora
                 }
             }
 		}
+
+        GenerateVisualStudioSolution();
 	}
+
+    static String GenerateVSStudioGUID(Random& rand)
+    {
+        String guid = "";
+
+        for (int i = 0; i < 8; i++)
+        {
+            guid += std::to_string(rand.Next() % 10);
+        }
+        guid += "-";
+        for (int i = 0; i < 4; i++)
+        {
+            guid += std::to_string(rand.Next() % 10);
+        }
+        guid += "-";
+        for (int i = 0; i < 4; i++)
+        {
+            guid += std::to_string(rand.Next() % 10);
+        }
+        guid += "-";
+        for (int i = 0; i < 4; i++)
+        {
+            guid += std::to_string(rand.Next() % 10);
+        }
+        guid += "-";
+        for (int i = 0; i < 12; i++)
+        {
+            guid += std::to_string(rand.Next() % 10);
+        }
+
+        return guid;
+    }
+
+    void CSharpProjectGenerator::GenerateVisualStudioSolution()
+    {
+        if (AssetManager::GetProjectAssetPath() == "")
+        {
+            return;
+        }
+
+        Random rand = Random(0);
+        String projects = "";
+        String configs = "";
+
+        std::filesystem::path path = AssetManager::GetProjectAssetPath() + "/../Code/CSharp";
+        if (std::filesystem::exists(path))
+        {
+            for (auto dir : std::filesystem::directory_iterator(path))
+            {
+                if (dir.is_directory())
+                {
+                    String guid = GenerateVSStudioGUID(rand);
+                    projects += "\
+Project(\"{" + GenerateVSStudioGUID(rand) + "}\") = \"" + dir.path().filename().string() + "\", \"" + (dir.path() / (dir.path().filename().string() + ".csproj")).string() + "\", \"{" + guid + "}\"\n\
+EndProject\n";
+                    configs = "\
+\t\t{" + guid + "}.Debug|Any CPU.ActiveCfg = Debug|Any CPU\n\
+\t\t{" + guid + "}.Debug|Any CPU.Build.0 = Debug|Any CPU\n\
+\t\t{" + guid + "}.Release|Any CPU.ActiveCfg = Release|Any CPU\n\
+\t\t{" + guid + "}.Release|Any CPU.Build.0 = Release|Any CPU\n";
+                }
+            }
+        }
+
+        String str = "\
+Microsoft Visual Studio Solution File, Format Version 12.00\n\
+# Visual Studio Version 17\n\
+Project(\"{9A19103F-16F7-4668-BE54-9A1E7A4F7556}\") = \"Coral.Managed\", \"" + AssetManager::GetEngineAssetPath() + "/../Code/Modules/CSharpScripting/ThirdParty/Coral/Coral.Managed/Coral.Managed.csproj\", \"{11178401-7DD7-FF37-4612-49B5B2914414}\"\n\
+EndProject\n\
+Project(\"{4A19103F-16F7-4668-BE54-9A1E7A4F7556}\") = \"Suora.Generated\", \"" + AssetManager::GetProjectAssetPath() + "/../Build/CSharp/Generated/Suora.Generated.csproj\", \"{31178401-7DD7-FF37-4612-49B5B2914414}\"\n\
+EndProject\n\
+Project(\"{2150E333-8FDC-42A3-9474-1A3956D46DE8}\") = \"SuoraEngine-Internal\", \"SuoraEngine-Internal\", \"{65CB7E83-D18B-FAB9-9AC6-433706463F96}\"\n\
+EndProject\n\
+" + projects + "\
+Global\n\
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution\n\
+		Debug|Any CPU = Debug|Any CPU\n\
+		Release|Any CPU = Release|Any CPU\n\
+	EndGlobalSection\n\
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution\n\
+		{11178401-7DD7-FF37-4612-49B5B2914414}.Debug|Any CPU.ActiveCfg = Debug|Any CPU\n\
+		{11178401-7DD7-FF37-4612-49B5B2914414}.Debug|Any CPU.Build.0 = Debug|Any CPU\n\
+		{11178401-7DD7-FF37-4612-49B5B2914414}.Release|Any CPU.ActiveCfg = Release|Any CPU\n\
+		{11178401-7DD7-FF37-4612-49B5B2914414}.Release|Any CPU.Build.0 = Release|Any CPU\n\
+		{31178401-7DD7-FF37-4612-49B5B2914414}.Debug|Any CPU.ActiveCfg = Debug|Any CPU\n\
+		{31178401-7DD7-FF37-4612-49B5B2914414}.Debug|Any CPU.Build.0 = Debug|Any CPU\n\
+		{31178401-7DD7-FF37-4612-49B5B2914414}.Release|Any CPU.ActiveCfg = Release|Any CPU\n\
+		{31178401-7DD7-FF37-4612-49B5B2914414}.Release|Any CPU.Build.0 = Release|Any CPU\n\
+" + configs + "\
+	EndGlobalSection\n\
+	GlobalSection(SolutionProperties) = preSolution\n\
+		HideSolutionNode = FALSE\n\
+	EndGlobalSection\n\
+	GlobalSection(NestedProjects) = preSolution\n\
+		{11178401-7DD7-FF37-4612-49B5B2914414} = {65CB7E83-D18B-FAB9-9AC6-433706463F96}\n\
+		{31178401-7DD7-FF37-4612-49B5B2914414} = {65CB7E83-D18B-FAB9-9AC6-433706463F96}\n\
+	EndGlobalSection\n\
+EndGlobal\
+";
+
+        std::filesystem::path slnPath = AssetManager::GetProjectAssetPath() + "/../";
+        slnPath /= ProjectSettings::Get()->GetAssetName() + "-CSharp.sln";
+
+        Platform::WriteToFile(slnPath.string(), str);
+
+    }
 
 }
--- Code/Modules/CSharpScripting/Source/CSharpProjectGenerator.h
+++ Code/Modules/CSharpScripting/Source/CSharpProjectGenerator.h
@@ -8,6 +8,7 @@ namespace Suora
 	{
 	public:
 		static void GenerateCSProjectFiles();
+		static void GenerateVisualStudioSolution();
 	};
 
 }
