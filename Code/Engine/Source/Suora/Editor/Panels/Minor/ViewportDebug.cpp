34d0a5a94324b8c76f112200cc062163f84a2d50
YanSchw <schoessow.yannick@gmail.com>
2024-03-28 02:12:05 +0100

    Update ViewportDebug.cpp

--- Code/Engine/Source/Suora/Editor/Panels/Minor/ViewportDebug.cpp
+++ Code/Engine/Source/Suora/Editor/Panels/Minor/ViewportDebug.cpp
@@ -98,6 +98,49 @@ namespace Suora
 			tr.SetRotation(cam->GetRotation());
 			tr.SetScale(Vec3(0.1f));
 			Renderer3D::DrawMesh(camera, cam->GetTransformMatrix(), *AssetManager::GetAsset<Mesh>(SuoraID("c37609b9-9067-4e3a-ac04-c493ed2d8009")), GizmoMaterial(HandlingMousePick, pickingID, Vec3(1.0f), SuoraID("317cf1ef-ac75-46d8-a62f-184891456960")), HandlingMousePick ? MaterialType::ObjectID : MaterialType::Material);
+
+			Vec3 corners[4];
+			{
+				Vec4 pos = glm::inverse(cam->GetViewProjectionMatrix()) * Vec4(-1.0f, 1.0f, 1.0f, 1.0f);
+				pos.x /= pos.w;
+				pos.y /= pos.w;
+				pos.z /= pos.w;
+				Vec3 end = glm::normalize(((Vec3)pos) - cam->GetPosition()) * 15.0f + cam->GetPosition();
+				corners[0] = end;
+				Renderer3D::DrawLine3D(camera, cam->GetPosition(), end, Color(0.66f, 1.0f, 0.66f, 0.5f));
+			}
+			{
+				Vec4 pos = glm::inverse(cam->GetViewProjectionMatrix()) * Vec4(1.0f, 1.0f, 1.0f, 1.0f);
+				pos.x /= pos.w;
+				pos.y /= pos.w;
+				pos.z /= pos.w;
+				Vec3 end = glm::normalize(((Vec3)pos) - cam->GetPosition()) * 15.0f + cam->GetPosition();
+				corners[1] = end;
+				Renderer3D::DrawLine3D(camera, cam->GetPosition(), end, Color(0.66f, 1.0f, 0.66f, 0.5f));
+			}
+			{
+				Vec4 pos = glm::inverse(cam->GetViewProjectionMatrix()) * Vec4(-1.0f, -1.0f, 1.0f, 1.0f);
+				pos.x /= pos.w;
+				pos.y /= pos.w;
+				pos.z /= pos.w;
+				Vec3 end = glm::normalize(((Vec3)pos) - cam->GetPosition()) * 15.0f + cam->GetPosition();
+				corners[2] = end;
+				Renderer3D::DrawLine3D(camera, cam->GetPosition(), end, Color(0.66f, 1.0f, 0.66f, 0.5f));
+			}
+			{
+				Vec4 pos = glm::inverse(cam->GetViewProjectionMatrix()) * Vec4(1.0f, -1.0f, 1.0f, 1.0f);
+				pos.x /= pos.w;
+				pos.y /= pos.w;
+				pos.z /= pos.w;
+				Vec3 end = glm::normalize(((Vec3)pos) - cam->GetPosition()) * 15.0f + cam->GetPosition();
+				corners[3] = end;
+				Renderer3D::DrawLine3D(camera, cam->GetPosition(), end, Color(0.66f, 1.0f, 0.66f, 0.5f));
+			}
+			Renderer3D::DrawLine3D(camera, corners[0], corners[1], Color(0.66f, 1.0f, 0.66f, 0.5f));
+			Renderer3D::DrawLine3D(camera, corners[1], corners[3], Color(0.66f, 1.0f, 0.66f, 0.5f));
+			Renderer3D::DrawLine3D(camera, corners[2], corners[0], Color(0.66f, 1.0f, 0.66f, 0.5f));
+			Renderer3D::DrawLine3D(camera, corners[3], corners[2], Color(0.66f, 1.0f, 0.66f, 0.5f));
+			
 			if (HandlingMousePick)
 			{
 				(*pickingMap)[*pickingID] = cam;
