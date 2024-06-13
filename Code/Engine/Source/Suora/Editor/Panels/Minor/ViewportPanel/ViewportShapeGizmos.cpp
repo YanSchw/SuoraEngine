782aa62046f411d8439bd69549eb4742a27fa0bd
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-06-13 18:18:31 +0200

    Added CapsuleShapeNode to JoltPhysics

--- Code/Engine/Source/Suora/Editor/Panels/Minor/ViewportPanel/ViewportShapeGizmos.cpp
+++ Code/Engine/Source/Suora/Editor/Panels/Minor/ViewportPanel/ViewportShapeGizmos.cpp
@@ -118,6 +118,42 @@ namespace Suora
 		}
 	}
 
+	static void Draw3DCapsule(CameraNode* InCamera, CapsuleShapeNode* InNode)
+	{
+		Array<std::pair<Vec3, Vec3>> lines = GenerateSphereTop(InNode->GetCapsuleRadius());
+		for (auto& line : lines)
+		{
+			line.first.y  += InNode->GetCapsuleHeight() / 2.0f;
+			line.second.y += InNode->GetCapsuleHeight() / 2.0f;
+		}
+		Array<std::pair<Vec3, Vec3>> lines2 = GenerateSphereTop(InNode->GetCapsuleRadius() * -1.0f);
+		for (auto& line : lines2)
+		{
+			line.first.y  -= InNode->GetCapsuleHeight() / 2.0f;
+			line.second.y -= InNode->GetCapsuleHeight() / 2.0f;
+		}
+		lines += lines2;
+
+		// Connecting both half spheres
+		const float HalfHeight = InNode->GetCapsuleHeight() / 2.0f;
+		lines.Add({ Vec3(-InNode->GetCapsuleRadius(), -HalfHeight, 0), Vec3(-InNode->GetCapsuleRadius(), +HalfHeight, 0) });
+		lines.Add({ Vec3(+InNode->GetCapsuleRadius(), -HalfHeight, 0), Vec3(+InNode->GetCapsuleRadius(), +HalfHeight, 0) });
+		lines.Add({ Vec3(0, -HalfHeight, -InNode->GetCapsuleRadius()), Vec3(0, +HalfHeight, -InNode->GetCapsuleRadius()) });
+		lines.Add({ Vec3(0, -HalfHeight, +InNode->GetCapsuleRadius()), Vec3(0, +HalfHeight, +InNode->GetCapsuleRadius()) });
+
+		Node3D proxy;
+		proxy.SetPosition(InNode->GetPosition());
+		proxy.SetRotation(InNode->GetRotation());
+		proxy.SetScale(Vec::One);
+
+		for (const std::pair<Vec3, Vec3>& line : lines)
+		{
+			const Vec4 a = proxy.GetTransformMatrix() * Vec4(line.first, 1);
+			const Vec4 b = proxy.GetTransformMatrix() * Vec4(line.second, 1);
+			Renderer3D::DrawLine3D(InCamera, a, b, Color(1, 0, 0, 1));
+		}
+	}
+
 	void ViewportShapeGizmos::DrawDebugGizmos(World* world, CameraNode* camera, int* pickingID, Map<int, Node*>* pickingMap, bool isHandlingMousePick)
 	{
 		Array<BoxShapeNode*> boxes = world->FindNodesByClass<BoxShapeNode>();
@@ -130,7 +166,13 @@ namespace Suora
 		{
 			Draw3DSphere(camera, sphere);
 		}
+		Array<CapsuleShapeNode*> capsules = world->FindNodesByClass<CapsuleShapeNode>();
+		for (CapsuleShapeNode* capsule : capsules)
+		{
+			Draw3DCapsule(camera, capsule);
+		}
 
+		/*
 		RenderCommand::SetWireframeMode(true);
 		RenderCommand::SetWireframeThickness(3.0f);
 		
@@ -150,7 +192,7 @@ namespace Suora
 			}
 		}
 		RenderCommand::SetWireframeMode(false);
-		RenderCommand::SetWireframeThickness(1.0f);
+		RenderCommand::SetWireframeThickness(1.0f);*/
 	}
 
 }
